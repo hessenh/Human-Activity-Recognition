427876e33ef18be6f891b9e4af5f8badaebe9ab3
hessenh <hansolavhessen@gmail.com>
2016-01-19 14:01:56 +0100

    Viterbi and some classifiers in sklearn

---
+++ python/Sklearn/Classifiers.py
@@ -0,0 +1,36 @@
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import GaussianNB
+
+import data_features
+
+
+def classify(classifier):
+	# Get data set
+	data = data_features.Data_Set()
+
+	# Set up classifier
+	if classifier == 'SVM':
+		clf = svm.SVC()
+	elif classifier == 'NearestCentroid':
+		clf = NearestCentroid()
+	elif classifier == 'RF':
+		clf = RandomForestClassifier(n_estimators = 100 )
+	elif classifier == 'SGD':
+		clf = SGDClassifier(loss="hinge", penalty="l2")
+	elif classifier == 'GNB':
+		clf = GaussianNB()
+	
+	# Fit the training data to the labels and create the decision trees
+	clf.fit(data.train_x,data.train_l)  
+
+	# Take the same decision trees and run it on the test data
+	#output = clf.predict(data.test_x)
+
+	# Calculate score with test data
+	score = clf.score(data.test_x,data.test_l)
+	print score
+
+classify('GNB')
