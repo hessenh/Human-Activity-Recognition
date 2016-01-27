61adc833a319721f1fadf16ef6cf527092d55f0a
Hans-Olav Hessen <hansolavhessen@gmail.com>
2016-01-27 17:16:36 +0100

    EM

--- python/Tensorflow/EM/CNN_TEST.py
+++ python/Tensorflow/EM/CNN_TEST.py
@@ -47,12 +47,14 @@ class CNN_TEST(object):
       self.cnn = CNN.CNN_TWO_LAYERS(config)
       self.cnn.set_data_set(self.data_set)
 
-      self.cnn.load_model('models/' + network_type + '_' + str(input_size))
+      self.cnn.load_model('models/' + network_type + '_' + str(input_size) + '_EM2')
       if complete_set:
          print self.cnn.test_network()
       else:
-         data = self.data_set.test.next_data_label(index)
-         print np.argmax(data[1])+1, self.cnn.run_network(data)
+         for i in range(0,100):
+            #print self.data_set.test.next_data_label(i)[1]
+            data = self.data_set.test.next_data_label(i)
+            print np.argmax(data[1])+1, self.cnn.run_network(data)
       
 
-cnn_h = CNN_TEST('sd', 2000, True, '1.5', 900)
+cnn_h = CNN_TEST('original', 2000, True, '1.5', 900)
