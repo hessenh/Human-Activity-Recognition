0cd57528e97f43133b8c892a2eb845579c9b8910
hessenh <hansolav_91@hotmail.com>
2016-02-19 12:50:22 +0100

    Testing different structures!

--- python/Tensorflow/exploring cnn/CNN_STATIC_VARIABLES.py
+++ python/Tensorflow/exploring cnn/CNN_STATIC_VARIABLES.py
@@ -1,7 +1,7 @@
 # Hierachy of Convolutional Neural Networks
 # 
 # ==============================================================================
-
+import numpy as np
 
 class CNN_STATIC_VARS(object):
 	''' Variables '''
@@ -31,21 +31,32 @@ class CNN_STATIC_VARS(object):
 		return len(set(convertion_list.values()))
 
 	''' Get config for CNN '''
-	def get_config(self, input_size, output_size, iterations, batch_size, model_name, conv_f_1, conv_f_2, nn_1, filter_type):
+	def get_config(self, input_size, output_size, iterations, batch_size, model_name, conv_f_1, conv_f_2, nn, filter_type):
 		return 	{
 		   'input_size': input_size, # Number of inputs 
 		   'output_size': output_size, # Number of ouptuts
 		   'iteration_size': iterations, # Number of training iterations
 		   'batch_size': batch_size, # Number of samples in each training iteration (batch)
-		   'model_name': model_name + '_' + str(input_size) + '_' + str(conv_f_1) + '_' + str(conv_f_2) + '_' + str(nn_1) + '_' + filter_type,
+		   'model_name': model_name + '_' + str(input_size) + '_' + str(conv_f_1) + '_' + str(conv_f_2) + '_' + str(nn[0]) + '_' +str(nn[1]) + '_' + filter_type,
 		   'conv_f_1': conv_f_1,
 		   'conv_f_2': conv_f_2,
-		   'nn_1': nn_1,
+		   'nn': nn,
 		   'filter_type': filter_type
 		}
 
 	''' Subject set '''
-	def get_subject_set(self):
-		TRAIN_SUBJECTS = ["06A","08A","09A"]#,"10A","11A","12A","13A","14A","15A","16A","18A","19A","20A","21A","22A","23A"]
-		TEST_SUBJECTS = ["01A","02A"]#,"03A","04A","05A"]
+	def get_subject_set(self, random):
+		SUBJECTS = ["01A","02A","03A","04A"]#,"05A","06A","08A","09A","10A","11A","12A","13A","14A","15A","16A","18A","19A","20A","21A","22A","23A"]
+		if random:
+			SUBJECTS = ["01A","02A","03A","04A","05A","06A","08A","09A","10A","11A","12A","13A","14A","15A","16A","18A","19A","20A","21A","22A","23A"]
+			TEST_SUBJECTS = []
+			# Test subjects is 1/3 of subject set
+			SUBJECTS_TEST_LENGTH = len(SUBJECTS) / 3
+			for i in range(0,SUBJECTS_TEST_LENGTH):
+				r = np.random.randint(len(SUBJECTS))
+				TEST_SUBJECTS.append(SUBJECTS.pop(r))
+			TRAIN_SUBJECTS = SUBJECTS
+		else:
+			TRAIN_SUBJECTS = ['01A', '02A', '04A', '05A', '06A', '08A', '09A', '11A', '12A', '13A', '15A', '16A', '19A', '23A']
+			TEST_SUBJECTS = ['21A', '20A', '14A', '18A', '03A', '22A', '10A']
 		return [TRAIN_SUBJECTS, TEST_SUBJECTS]
